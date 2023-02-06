faf216abfd58adc66e85152d0f24b068393732c8
Debaditya Bhar <debaditya.bhar@outlook.com>
2023-02-06 19:02:54 +0530

    Version 2.0 - Created following functionalities:
    	1. Use 'jimp' library to create black and white thumbnails of
    	   the images stored in 'Pictures/' S3 object (folder).
    
    	2. Upload the black and white thumbnail image to the amazon S3
    	   object (folder) called 'Thumbnail/'.

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
-const awsS3 = require("@aws-sdk/client-s3");
-const SignURL = require("@aws-sdk/s3-request-presigner");
-const fs = require("fs");
-const Jimp = require("jimp");
+// Amazon S3 modules.
+import {
+  S3Client,
+  CreateBucketCommand,
+  DeleteBucketCommand,
+  PutObjectCommand,
+  GetObjectCommand,
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-// The contants.
+// Additional modules
+import fs from "fs";
+import Jimp from "jimp";
+
+// The constant literals.
 const REGION = "ap-south-1"; // Mumbai
 const BucketName = "picture-thumbnail-bucket";
 
 // Create an Amazon S3 service client object.
-const s3Client = new awsS3.S3Client({ region: REGION });
+const s3Client = new S3Client({ region: REGION });
+
+// Amazon S3 related functions.
 
 // This function creates amazon S3 bucket.
+// Input: Bucket Name.
+// Output: None.
+
 async function createS3Bucket(BucketName) {
   // Prepare Create Bucket Command.
-  let cmd = new awsS3.CreateBucketCommand({ Bucket: BucketName });
+  let cmd = new CreateBucketCommand({ Bucket: BucketName });
 
   // Create amazon S3 Bucket.
   try {
@@ -28,9 +42,12 @@ async function createS3Bucket(BucketName) {
 }
 
 // This function deletes amazon S3 bucket.
+// Input: Bucket Name.
+// Output: None.
+
 async function deleteS3Bucket(BucketName) {
   // Prepare delete bucket command.
-  let cmd = new awsS3.DeleteBucketCommand({ Bucket: BucketName });
+  let cmd = new DeleteBucketCommand({ Bucket: BucketName });
 
   // Delete amazon S3 Bucket.
   try {
@@ -45,44 +62,50 @@ async function deleteS3Bucket(BucketName) {
 }
 
 // This function uploads a file into amazon S3 bucket.
-async function uploadS3Object(BucketName, ObjectKey, FilePath) {
-  // Create the read stream form the file path.
-  let FileContent = fs.createReadStream(FilePath);
+// Input: Bucket Name, Object Key and File content as ReadableStream.
+// Output: None.
 
+async function uploadS3Object(BucketName, ObjectKey, FileContent) {
   // Prepare the put object command.
   let param = {
     Bucket: BucketName,
     Key: ObjectKey,
     Body: FileContent,
   };
-  let cmd = new awsS3.PutObjectCommand(param);
+  let cmd = new PutObjectCommand(param);
 
   // Upload amazon S3 object.
   try {
     const response = await s3Client.send(cmd);
-    console.log(`\n${ObjectKey} is uploaded into ${BucketName} successfully.`);
+    console.log(
+      `\nFile uploaded successfully!\nBucket - ${BucketName} | Object Key - ${ObjectKey}\n`
+    );
   } catch (error) {
     console.log("\nERROR!");
     console.log(`Bucket Name: ${BucketName}\nError Code: ${error.Code}\n`);
+    console.log(error);
   }
 }
 
-// This function gets a file content as stream from amazon S3 bucket.
+// This function gets a pre-signed URL of amazon S3 bucket object.
+// Input: Bucket Name, Object Key.
+// Output: Pre-signed URL.
+
 async function getS3ObjectURL(BucketName, ObjectKey) {
   // Prepare the get object command.
   let param = {
     Bucket: BucketName,
     Key: ObjectKey,
   };
-  let cmd = new awsS3.GetObjectCommand(param);
+  let cmd = new GetObjectCommand(param);
 
   // Get amazon S3 object Pre-Signed URL.
   try {
     // Create the presigned URL.
-    const signedUrl = await SignURL.getSignedUrl(s3Client, cmd, {
+    const signedUrl = await getSignedUrl(s3Client, cmd, {
       expiresIn: 3600,
     });
-    console.log(`\n${ObjectKey} URL - ${signedUrl}.`);
+    console.log(`\n${ObjectKey} URL - ${signedUrl}.\n`);
     return signedUrl;
   } catch (error) {
     console.log("\nERROR!");
@@ -93,7 +116,23 @@ async function getS3ObjectURL(BucketName, ObjectKey) {
 
 // Additional Helper functions.
 
-// This function converts the steam into Buffer
+// This function creates black and white thumbnail of image.
+// Input: Image URL in S3 bucket.
+// Output: The transformed image stream.
+
+async function makeBWThumbnail(ImageURL) {
+  const TempWriteFilePath = "tmp/dummy.jpg";
+  try {
+    let InputImage = await Jimp.read(ImageURL);
+    let NewImage = InputImage.resize(256, Jimp.AUTO).grayscale().quality(80);
+    await NewImage.writeAsync(TempWriteFilePath);
+    let Content = fs.createReadStream(TempWriteFilePath);
+    return Content;
+  } catch (error) {
+    console.log("\nImage Processing Error!\n");
+    console.log(error);
+  }
+}
 
 // Main execution.
 
@@ -101,20 +140,23 @@ async function main() {
   // await createS3Bucket(BucketName);
   // await deleteS3Bucket(BucketName);
 
-  let FilePath = "./Pictures/33.jpg";
-  let ObjectKey = `${FilePath.split("/")[1]}/${FilePath.split("/")[2]}`;
-  let OutFileName = `./Pictures/output/${
-    FilePath.split("/")[2].split(".")[0]
-  }_BW.jpg`;
+  let FilePath = "Pictures/23.jpg";
+  let ObjectKey = FilePath;
+  let OutObjKey = `Thumbnail/${
+    FilePath.split("/")[1].split(".")[0]
+  }_BW_Thumbnail.jpg`;
 
-  // await uploadS3Object(BucketName, ObjectKey, FilePath);
+  // Create the read stream form the file path.
+  // let FileContent = fs.createReadStream(FilePath);
+  // await uploadS3Object(BucketName, ObjectKey, FileContent);
 
+  // Get the image URL.
   let FileURL = await getS3ObjectURL(BucketName, ObjectKey);
-  Jimp.read(FileURL, (err, pic) => {
-    if (err) throw err;
-    pic.grayscale().write(OutFileName);
-    console.log(`\nThe Black and White Image saved in ${OutFileName}\n`);
-  });
+  // Get the Thumbnail Content.
+  let FileContent = await makeBWThumbnail(FileURL);
+  if (FileContent) {
+    await uploadS3Object(BucketName, OutObjKey, FileContent);
+  }
 }
 
 main();
